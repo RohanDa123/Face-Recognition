ae76a264d4296c9fee3c1dab4625a15c5ee85d3d
RohanDa123 <77546484+RohanDa123@users.noreply.github.com>
2021-01-16 10:01:29 -0800

    Add files via upload

---
+++ camarabootcamp.py
@@ -0,0 +1,34 @@
+import cv2
+
+cap=cv2.videoCapture(0)
+
+face_cascade=cv2.cascadeClassifier("haarcascade_frontalface_alt.xml")
+
+while True:
+	ret,frame=cap.read()
+	gray_frame=cv2.cvtColour(frame,cv2.COLOUR_BGR2GRAY)
+
+
+	if ret==False:
+		continue
+
+	face=face_cascade.detectMultiScale(gray_frame,1.3,5)
+	print(faces)
+
+
+	for(x,y,w,h) in faces:
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+	cv2.imshow("Video Frame",frame)
+
+
+	key_pressed=cv2.waitkey(1) & 0xFF
+    
+    if key_pressed==ord('q');
+	    break
+
+
+
+
+cap.release()
+cv2.destroyAllWindows()
